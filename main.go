467485f5c68c1a77da6f0a15f4b462c1e708a413
Ian Velder <icvelder@gmail.com>
2026-03-18 20:51:46 -0500

    polishing cli, improving input validation

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/CodeZeroSugar/gocrypt/internal/app"
@@ -11,19 +10,38 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	if len(args) != 2 {
-		log.Println("Not enough arguments provided")
+	if len(args) != 3 {
+		fmt.Print("\nNot enough arguments provided")
+		return
 	}
 
 	fmt.Print("Enter secret:")
 	password, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
-		log.Fatal("\nError reading input:", err)
+		fmt.Print("\nError reading input:", err)
+		return
 	}
-	path := args[1]
-	data, err := os.ReadFile(path)
+
+	fmt.Print("\nConfirm secret:")
+	confirm, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
-		log.Fatal("Could not read provided file.")
+		fmt.Print("\nError reading input:", err)
+		return
+	}
+
+	if string(password) != string(confirm) {
+		fmt.Print("\nConfirmation did not match password")
+		return
+
+	}
+
+	inPath := args[1]
+	outPath := args[2]
+
+	data, err := os.ReadFile(inPath)
+	if err != nil {
+		fmt.Print("Could not read provided file.")
+		return
 	}
 
 	switch command := args[0]; command {
@@ -31,25 +49,37 @@ func main() {
 
 		encrypted, err := app.CommandEncrypt(data, password)
 		if err != nil {
-			log.Fatal("\nFailed to encrypt input:", err)
+			fmt.Print("\nFailed to encrypt input:", err)
+			return
 		}
 
-		err = os.WriteFile(path, encrypted, 0o644)
+		outPath += ".gcrypt"
+		err = os.WriteFile(outPath, encrypted, 0o644)
 		if err != nil {
-			log.Fatal("\nFailed to write encrypted file:", err)
+			fmt.Print("\nFailed to write encrypted file:", err)
+			return
 		}
+
+		fmt.Println("Successfully encrypted file:", outPath)
 
 	case "decrypt":
 
 		decrypted, err := app.CommandDecrypt(data, password)
 		if err != nil {
-			log.Fatal("\nFailed to decrypt input:", err)
+			fmt.Print("\nFailed to decrypt input:", err)
+			return
 		}
 
-		err = os.WriteFile(path, decrypted, 0o664)
+		err = os.WriteFile(outPath, decrypted, 0o664)
 		if err != nil {
-			log.Fatal("\nFailed to write plaintext file:", err)
+			fmt.Print("\nFailed to write plaintext file:", err)
+			return
 		}
 
+		fmt.Println("Successfully decrypted file:", outPath)
+
+	default:
+		fmt.Printf("\n'%s' is not a valid command", command)
+		return
 	}
 }
